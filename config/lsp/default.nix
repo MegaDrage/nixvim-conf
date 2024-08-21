01a7e1c8b6c7806852090bd116f415c626d81d98
MegaDrage <dragonpsui@gmail.com>
2024-08-21 22:57:36 +0700

    Try to make autosave

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -23,20 +23,14 @@
         "gi" = "implementation";
         "K" = "hover";
       };
-      extraConfigLua = ''
-          on_attach = function(client, bufnr)
-            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-            if client.supports_method("textDocument/formatting") then
-              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-              vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                  vim.lsp.buf.format({ async = true })  -- Асинхронное форматирование
-                end,
-              })
-            end
+      postConfig = ''
+        -- Создаем автокоманду для автоформатирования при сохранении
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*",  -- Можно указать конкретные типы файлов, например, "*.lua", "*.js" и т.д.
+          callback = function()
+            vim.lsp.buf.format({ async = true })  -- Асинхронное форматирование
           end,
+        })
       '';
     };
     rust-tools.enable = true;
