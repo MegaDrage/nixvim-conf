6770b0c48e1249125d29d303a3c40f17cc1ebafb
MegaDrage <dragonpsui@gmail.com>
2024-08-21 21:11:15 +0700

    Upd bufferline n todo-comments settings

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,19 +1,21 @@
 {
   plugins.bufferline = {
     enable = true;
-    options = {
-      mode = "buffers";
-      indicator = {
-        style = "icon";
-        icon = "  ";
+    settings = {
+      options = {
+        mode = "buffers";
+        indicator = {
+          style = "icon";
+          icon = "  ";
+        };
+        diagnostics = "nvim_lsp";
+        offsets = [{
+          fileType = "neo-tree";
+          text = "File Explorer";
+          separator = true;
+          padding = 1;
+        }];
       };
-      diagnostics = "nvim_lsp";
-      offsets = [{
-        fileType = "neo-tree";
-        text = "File Explorer";
-        separator = true;
-        padding = 1;
-      }];
     };
   };
 }
--- config/utils/todo-comments.nix
+++ config/utils/todo-comments.nix
@@ -1,13 +1,15 @@
 {
   plugins.todo-comments = {
     enable = true;
-    colors = {
-      error = [ "DiagnosticError" "ErrorMsg" "#DC2626" ];
-      warning = [ "DiagnosticWarn" "WarningMsg" "#FBBF24" ];
-      info = [ "DiagnosticInfo" "#2563EB" ];
-      hint = [ "DiagnosticHint" "#10B981" ];
-      default = [ "Identifier" "#7C3AED" ];
-      test = [ "Identifier" "#FF00FF" ];
+    settings = {
+      colors = {
+        error = [ "DiagnosticError" "ErrorMsg" "#DC2626" ];
+        warning = [ "DiagnosticWarn" "WarningMsg" "#FBBF24" ];
+        info = [ "DiagnosticInfo" "#2563EB" ];
+        hint = [ "DiagnosticHint" "#10B981" ];
+        default = [ "Identifier" "#7C3AED" ];
+        test = [ "Identifier" "#FF00FF" ];
+      };
     };
   };
 }
