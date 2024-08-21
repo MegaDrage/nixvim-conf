9047637c0bcb1664f2c36207ffb75e226d79f88f
MegaDrage <dragonpsui@gmail.com>
2024-08-21 21:07:05 +0700

    Upd bufferline settings

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,18 +1,19 @@
 {
   plugins.bufferline = {
     enable = true;
-    mode = "buffers";
-    indicator = {
-      style = "icon";
-      icon = "  ";
+    options = {
+      mode = "buffers";
+      indicator = {
+        style = "icon";
+        icon = "  ";
+      };
+      diagnostics = "nvim_lsp";
+      offsets = [{
+        fileType = "neo-tree";
+        text = "File Explorer";
+        separator = true;
+        padding = 1;
+      }];
     };
-    diagnostics = "nvim_lsp";
-    offsets = [{
-      fileType = "neo-tree";
-      text = "File Explorer";
-      separator = true;
-      padding = 1;
-    }
-    ];
   };
 }
