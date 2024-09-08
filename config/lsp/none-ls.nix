5e0f46296b8c51ac457346602d97e70e0d304354
MegaDrage <dragonpsui@gmail.com>
2024-09-08 15:49:01 +0700

    config/lsp/none-ls.nix: rm auto-fmt on save

--- config/lsp/none-ls.nix
+++ config/lsp/none-ls.nix
@@ -21,22 +21,22 @@
         shfmt.enable = true;
       };
     };
-    settings = {
-      on_attach = ''
-        function(client, bufnr)
-            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-            if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = augroup,
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format({ async = false })
-                    end,
-                })
-            end
-        end
-      '';
-    };
+    # settings = {
+    #   on_attach = ''
+    #     function(client, bufnr)
+    #         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    #         if client.supports_method("textDocument/formatting") then
+    #             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    #             vim.api.nvim_create_autocmd("BufWritePre", {
+    #                 group = augroup,
+    #                 buffer = bufnr,
+    #                 callback = function()
+    #                     vim.lsp.buf.format({ async = false })
+    #                 end,
+    #             })
+    #         end
+    #     end
+    #   '';
+    # };
   };
 }
