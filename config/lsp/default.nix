ae6e8ffd6cf008171bea65aebdf8bc2b6647a529
MegaDrage <dragonpsui@gmail.com>
2024-10-22 13:38:51 +0700

    config/lsp: fix servers names

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -5,15 +5,15 @@
       enable = true;
       servers = {
         bashls.enable = true;
-        jdt-language-server.enable = true;
+        jdtls.enable = true;
         clangd.enable = true;
         elixirls.enable = true;
         gleam.enable = true;
         gopls.enable = true;
-        kotlin-language-server.enable = true;
+        kotlin_language_server.enable = true;
         nixd.enable = true;
-        ruff-lsp.enable = true;
-        nil-ls.enable = true;
+        ruff_lsp.enable = true;
+        nil_ls.enable = true;
         marksman.enable = true;
       };
       keymaps.lspBuf = {
