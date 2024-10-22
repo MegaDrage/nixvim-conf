28c20ea97c49c4d4773a9f511b23efec048816f5
MegaDrage <dragonpsui@gmail.com>
2024-10-22 14:08:38 +0700

    config/: try other cs

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,4 @@
-{
+{ config, lib, ... }: {
   imports = [
     ./bufferline.nix
     ./cmp.nix
@@ -13,7 +13,32 @@
     ./utils
   ];
 
-  colorschemes.dracula.enable = true;
+  colorschemes.base16 = {
+    enable = true;
+    # colorscheme = "onedark";
+    colorscheme = {
+      base00 = "${config.lib.stylix.colors.base00}";
+      base01 = "${config.lib.stylix.colors.base01}";
+      base02 = "${config.lib.stylix.colors.base02}";
+      base03 = "${config.lib.stylix.colors.base03}";
+      base04 = "${config.lib.stylix.colors.base04}";
+      base05 = "${config.lib.stylix.colors.base05}";
+      base06 = "${config.lib.stylix.colors.base06}";
+      base07 = "${config.lib.stylix.colors.base07}";
+      base08 = "${config.lib.stylix.colors.base08}";
+      base09 = "${config.lib.stylix.colors.base09}";
+      base0A = "${config.lib.stylix.colors.base0A}";
+      base0B = "${config.lib.stylix.colors.base0B}";
+      base0C = "${config.lib.stylix.colors.base0C}";
+      base0D = "${config.lib.stylix.colors.base0D}";
+      base0E = "${config.lib.stylix.colors.base0E}";
+      base0F = "${config.lib.stylix.colors.base0F}";
+    };
+    settings = {
+      telescope = true;
+      telescope_borders = true;
+    };
+  };
 
   diagnostics = { virtual_lines.only_current_line = true; };
 
