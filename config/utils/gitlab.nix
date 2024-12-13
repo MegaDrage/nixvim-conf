ed7c3b1f92885f3be8534fb9ffa36948ae977dd5
MegaDrage <dragonpsui@gmail.com>
2024-12-13 15:41:18 +0700

    config/utils: gitlab ext

--- config/utils/gitlab.nix
+++ config/utils/gitlab.nix
@@ -1,1 +1,13 @@
-{ plugins.gitlab = { enable = true; }; }
+{
+  plugins.gitlab = {
+    enable = true;
+    settings = {
+      code_suggestions = {
+        auto_filetypes =
+          [ "ruby" "typescript" "typescriptreact" "sql" "sh" "javascript" ];
+      };
+      gitlab_url = "https://gitlab.makeshop.pro";
+      statuslines.enable = true;
+    };
+  };
+}
